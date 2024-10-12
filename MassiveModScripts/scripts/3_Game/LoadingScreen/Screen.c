269ceb2a232f5cd7704d7d6454a7abf5bcfc7b03
MassPwnaged <48640714+MassPwnaged@users.noreply.github.com>
2024-10-12 09:09:11 -0500

    Update Screen.c

--- MassiveModScripts/scripts/3_Game/LoadingScreen/Screen.c
+++ MassiveModScripts/scripts/3_Game/LoadingScreen/Screen.c
@@ -1,140 +1,357 @@
-modded class LoadingScreen 
-{
-    void LoadingScreen(DayZGame game) 
-    {
-        Class.CastTo(m_ImageLogoMid, m_WidgetRoot.FindAnyWidget("ImageLogoMid"));
-        Class.CastTo(m_ImageLogoCorner, m_WidgetRoot.FindAnyWidget("ImageLogoCorner"));
-        Class.CastTo(m_ModdedWarning, m_WidgetRoot.FindAnyWidget("ModdedWarning"));
-        m_ImageLogoMid.Show(false);
-        m_ImageLogoCorner.Show(true);
-        m_ModdedWarning.Show(false);        
+/**
+ * ExpansionRespawnHandlerModule.c
+ *
+ * DayZ Expansion Mod
+ * www.dayzexpansion.com
+ * © 2022 DayZ Expansion Mod Team
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/.
+ *
+*/
 
-
-        string port;
-        GetCLIParam("port", port);
-        if (port == "2504")
-        {
-            m_ImageBackground.LoadImageFile(0, "MassiveModScripts/Data/chernarus.paa");
-        }
-        m_ImageBackground.LoadMaskTexture("MassiveModScripts/Data/chernarus.paa");
-        ProgressAsync.SetUserData(m_ImageBackground);
-    }
-	
-	//void Show()
-	//{
-	//	Widget lIcon = m_ImageBackground;
-	//	Widget pText = m_ProgressLoading;
-	//	ref TStringArray RandomQuotes = 
-	//	{
-	//		"Nothing is more dangerous than a Man with nothing to lose.",
-	//		//"Drinking Disinfectant does not cure the Sniffles.",
-	//		//"Spawn, Freeze, Starve, Repeat.",
-	//		//"Everyone is Friendly in Vorkuta",
-	//		//"Everyone is Friendly in Cherno",
-	//		//"Look for big Red signs titled 'Trader' They lead somewhere useful.",
-	//		"Check out the Server Information panel by pressing escape and clicking the button.",
-	//		"You can cut down a small bush for wood with your hands or knife",
-	//		"You can Light a fire with a road flare",
-	//		//"Dropping frozen items on the ground next to a fire will thaw them.",
-	//		"You can Scrap Guns with a Screwdriver and use the scrap to make a advanced repair kit",
-	//		//"Only Tier 4 Caches drop Tier 5 Keys",
-	//		//"You can Combine Duplicate Keys to create a tier higher than the duplicate.",
-	//		//"keep your healthbar above 0 to survive",
-	//		//"Tier 5 keys can only be used on Namalsk",
-	//		"You can access a person's inventory if their hands are up.",
-	//		"Unlike in Vanilla DayZ, Apartments spawn loot.",
-	//		"Karma DOES Exist.",
-	//		"Protip: During the Winter, Eating food above 70 degrees will damage you, but any food 40-70 degrees will help give you Heat+ faster than just standing by a fire.",
-	//		"Protip: The MRE Waterless-Heater can heat foods fast, and when those foods are placed in a jacket, they will keep you far warmer than normal Heat Packs.",
-	//		"Protip: Just because your Bottle and Apple are filled, it doesn't mean your Hydration and Energy are. Eat until the icons flash a chevron up and down for the True-Full state.",
-	//		"Protip: Multivitamins will add 50% to your immunity stat, preventing illness even around other players sick with the Flu. When combined with Tetra, they will cure you of the flu faster.",
-	//		"Protip: Look for clothing labeled as 'Best Insulation'. These items will keep you warmer in the cold; when wet, they will lose their insulating properties, so be sure to wring them out often.",
-	//		"Protip: When hunting, try to shoot animals in the head, as body damage will result in a lower yield of meat and animal parts.",
-	//		"Protip: Wolves will chase meat for 20 seconds once thrown, so use that time to get away. Wolves lose interest in targets 200m away. Gotta go fast.",
-	//		//"This Pleases the Lord",
-	//		//"I made this loading screen - Mass",
-	//		"There is no Honor in the wasteland"
-	//	};
-	//	
-	//	//imageWidgetBackground.LoadImageFile(0, loadingScreenImagesNamalsk.GetRandomElement());// double check that function name
-    //    m_ProgressText.SetText(RandomQuotes.GetRandomElement());
-    //    m_ModdedWarning.SetText(RandomQuotes.GetRandomElement());
-	//	
-	//	m_ProgressLoading.SetCurrent( 0.0 );
-	//	m_ImageBackground.SetMaskProgress( 0.0 );
-	//	if ( !m_WidgetRoot.IsVisible() )
-	//	{
-	//		if ( m_DayZGame.GetUIManager().IsDialogVisible() )
-	//		{
-	//			m_DayZGame.GetUIManager().HideDialog();
-	//		}
-	//		
-	//		if ( m_DayZGame.GetMissionState() == DayZGame.MISSION_STATE_MAINMENU )
-	//		{
-	//			m_ImageWidgetBackground.Show( true );		
-	//			m_TextWidgetStatus.Show(true);
-	//		}
-	//		else
-	//		{
-	//			m_ImageWidgetBackground.Show( true );
-	//			m_TextWidgetStatus.Show(false);
-	//		}
-	//		
-	//		m_WidgetRoot.Show( true );
-	//		m_TextWidgetTitle.SetText( "" );
-	//		m_TextWidgetStatus.SetText( "" );
-	//	}
-	//	
-	//	ProgressAsync.SetProgressData(pText);
-	//	ProgressAsync.SetUserData(lIcon);
-	//}
-}
-modded class LoginQueueBase 
-{	
-	override Widget Init() 
-	{
-	    Widget w = super.Init();
-		ImageWidget imageWidgetBackground = ImageWidget.Cast( layoutRoot.FindAnyWidget("Background") );
-	    imageWidgetBackground.LoadMaskTexture("");
-		
-		
-		
-		string worldname;
-        GetGame().GetWorldName(worldname);
-        worldname.ToLower(); //to be safe 
-       // if ( worldname == "namalsk" )
-       // {
-	   //     imageWidgetBackground.LoadImageFile(0, "MassiveModScripts/Data/namalsk.paa");// double check that function name
-       // }
-		if ( worldname == "chernarus" )
-        {
-	        imageWidgetBackground.LoadImageFile(0, "MassiveModScripts/Data/chernarus.paa");// double check that function name
-        }
-		return w;
-	}
-}
-modded class LoginTimeBase 
+class CustomLoadingScreenBackground
 {
-	
-    override Widget Init() 
+	string ImagePath;
+};
+
+class CustomLoadingScreenData
+{
+	bool ShowLogo = true;
+	string LogoPath = "LoadingScreen/GUI/logo.edds";
+	bool RandomizeBackgounds = true;
+	int LoadingBarColor = ARGB(255, 199, 38, 81);
+	bool UseCustomHints = true;
+	string HintIconPath = "LoadingScreen/GUI/icons/circle_info.edds";
+
+	ref array<ref CustomLoadingScreenBackground> m_ExBackgrounds;
+
+	void CustomLoadingScreenData()
 	{
-	    Widget w = super.Init();
-		ImageWidget imageWidgetBackground = ImageWidget.Cast( layoutRoot.FindAnyWidget("Background") );
-	    imageWidgetBackground.LoadMaskTexture("");
-		
-		
-		
-		string worldname;
-        GetGame().GetWorldName(worldname);
-        worldname.ToLower(); //to be safe 
-       // if ( worldname == "namalsk" )
-       // {
-	   //     imageWidgetBackground.LoadImageFile(0, "MassiveModScripts/Data/namalsk.paa");// double check that function name
-       // }
-		if ( worldname == "chernarus" )
-        {
-	        imageWidgetBackground.LoadImageFile(0, "MassiveModScripts/Data/chernarus.paa");// double check that function name
-        }
-		return w;
+		m_ExBackgrounds = new array<ref CustomLoadingScreenBackground>;
+		JsonFileLoader<array<ref CustomLoadingScreenBackground>>.JsonLoadFile("LoadingScreen/Scripts/Data/LoadingImages.json", m_ExBackgrounds);
 	}
-}
+};
+
+modded class UiHintPanel
+{
+	protected const string 	m_ExDataPath = "LoadingScreen/Scripts/Data/Hints.json"; //! Custom hints json path
+	protected ref CustomLoadingScreenData m_ExCustomLoadingScreenData;
+
+	override protected void LoadContentList()
+	{
+		if (!m_ExCustomLoadingScreenData)
+			m_ExCustomLoadingScreenData = new CustomLoadingScreenData();
+
+		if (m_ExCustomLoadingScreenData && m_ExCustomLoadingScreenData.UseCustomHints)
+		{
+			JsonFileLoader<array<ref HintPage>>.JsonLoadFile(m_ExDataPath, m_ContentList);
+		}
+		else
+		{
+			JsonFileLoader<array<ref HintPage>>.JsonLoadFile(m_DataPath, m_ContentList);
+		}
+	}
+};
+
+modded class LoginScreenBase
+{
+	protected ImageWidget m_ExHintIcon;
+	protected int m_ExBackgroundIndex = 0;
+
+	protected const float LOADING_SCREEN_CHANGE_TIME = 3.0;
+	protected float m_ExLoadingTime;
+
+	protected ImageWidget m_ExBackground;
+
+	protected ref CustomLoadingScreenData m_ExCustomLoadingScreenData;
+	protected ref array<int> m_ShownBackgrounds;
+
+	void LoginScreenBase()
+	{
+		m_ExCustomLoadingScreenData = new CustomLoadingScreenData();
+
+		if (!m_ExCustomLoadingScreenData)
+			return;
+
+		if (m_ExCustomLoadingScreenData.RandomizeBackgounds)
+			m_ShownBackgrounds = new array<int>;
+	}
+
+	/*override Widget Init()
+	{
+		layoutRoot = super.Init();
+
+		m_ExBackground = ImageWidget.Cast(layoutRoot.FindAnyWidget("Background"));
+
+		SetBackgroundImage(m_ExBackgroundIndex);
+		m_ExBackgroundIndex++;
+
+		return layoutRoot;
+	}*/
+
+	override void Update(float timeslice)
+	{
+		super.Update(timeslice);
+
+		m_ExLoadingTime += timeslice;
+		if (m_ExLoadingTime >= LOADING_SCREEN_CHANGE_TIME)
+		{
+			if (!m_ExCustomLoadingScreenData.RandomizeBackgounds)
+			{
+				SetBackgroundImage(m_ExBackgroundIndex);
+				m_ExBackgroundIndex++;
+				if (m_ExBackgroundIndex > (m_ExCustomLoadingScreenData.m_ExBackgrounds.Count() - 1))
+					m_ExBackgroundIndex = 0;
+			}
+			else
+			{
+				if (m_ShownBackgrounds.Count() < m_ExCustomLoadingScreenData.m_ExBackgrounds.Count())
+				{
+					m_ExBackgroundIndex = GetRandomBackroundIndex();
+					SetBackgroundImage(m_ExBackgroundIndex);
+					m_ShownBackgrounds.Insert(m_ExBackgroundIndex);
+				}
+				else
+				{
+					m_ShownBackgrounds.Clear();
+				}
+
+			}
+
+			m_ExLoadingTime = 0;
+		}
+	}
+
+	protected int GetRandomBackroundIndex()
+	{
+		int index = Math.RandomIntInclusive(0, (m_ExCustomLoadingScreenData.m_ExBackgrounds.Count() - 1));
+		int searchCount;
+		while ((m_ShownBackgrounds.Find(index) > -1) && (searchCount < m_ShownBackgrounds.Count()))
+		{
+			index = Math.RandomIntInclusive(0, (m_ExCustomLoadingScreenData.m_ExBackgrounds.Count() - 1));
+		}
+
+		return index;
+	}
+
+	protected void SetBackgroundImage(int index)
+	{
+		CustomLoadingScreenBackground background = m_ExCustomLoadingScreenData.m_ExBackgrounds.Get(index);
+		if (background && background.ImagePath != string.Empty)
+		{
+			Print(ToString() + "::SetBackgroundImage - Background: " + background.ImagePath);
+			m_ExBackground.LoadImageFile(0, background.ImagePath, true);
+			m_ExBackground.SetImage(0);
+		}
+	}
+};
+
+modded class LoginQueueBase
+{
+	override Widget Init()
+	{
+		layoutRoot = super.Init();
+
+		m_ExBackground = ImageWidget.Cast(layoutRoot.FindAnyWidget("Background"));
+
+		SetBackgroundImage(m_ExBackgroundIndex);
+		m_ExBackgroundIndex++;
+
+		return layoutRoot;
+	}
+};
+
+modded class LoginTimeBase
+{
+	override Widget Init()
+	{
+		layoutRoot = super.Init();
+
+		m_ExBackground = ImageWidget.Cast(layoutRoot.FindAnyWidget("Background"));
+
+		SetBackgroundImage(m_ExBackgroundIndex);
+		m_ExBackgroundIndex++;
+
+		return layoutRoot;
+	}
+};
+
+modded class LoadingScreen
+{
+	protected Widget m_ExPanelWidget;
+	protected ImageWidget m_VanillaHintIcon;
+	protected ImageWidget m_ExHintIcon;
+	protected int m_ExBackgroundIndex = 0;
+
+	protected const float LOADING_SCREEN_CHANGE_TIME = 2.5;
+	protected float m_ExLoadingTime;
+	protected float m_ExLoadingTimeStamp;
+
+	protected ref CustomLoadingScreenData m_ExCustomLoadingScreenData;
+	protected ref array<int> m_ShownBackgrounds;
+
+	void LoadingScreen(DayZGame game)
+	{
+		m_ExCustomLoadingScreenData = new CustomLoadingScreenData();
+		if (!m_ExCustomLoadingScreenData)
+			return;
+
+		if (m_ExCustomLoadingScreenData.RandomizeBackgounds)
+			m_ShownBackgrounds = new array<int>;
+
+		m_ExPanelWidget = Widget.Cast(m_WidgetRoot.FindAnyWidget("PanelWidget1"));
+
+		//! Hide vanilla hint icon
+		float posHintIconX, posHintIconY;
+		m_VanillaHintIcon = ImageWidget.Cast(m_WidgetRoot.FindAnyWidget("hintIcon"));
+		m_VanillaHintIcon.GetScreenPos(posHintIconX, posHintIconY);
+		m_VanillaHintIcon.Show(false);
+		m_VanillaHintIcon.Unlink();
+
+		//! Create custom hint icon
+		m_ExHintIcon = ImageWidget.Cast(game.GetLoadingWorkspace().CreateWidgets("LoadingScreen/GUI/layouts/hint_icon.layout", m_ExPanelWidget));
+		m_ExHintIcon.SetScreenPos(posHintIconX, posHintIconY, true);
+		m_ExHintIcon.LoadImageFile(0, m_ExCustomLoadingScreenData.HintIconPath);
+
+		//! Remove vanilla mask texture
+		m_ImageWidgetBackground.LoadMaskTexture("");
+
+		//! Replace and set logo
+		if (m_ExCustomLoadingScreenData.ShowLogo && m_ExCustomLoadingScreenData.LogoPath != string.Empty)
+		{
+			float posLogoX, posLogoY;
+			m_ImageLogoCorner.GetScreenPos(posLogoX, posLogoY);
+			m_ImageLogoCorner.Show(false);
+			m_ImageLogoCorner.Unlink();
+
+			m_ImageLogoCorner = ImageWidget.Cast(game.GetLoadingWorkspace().CreateWidgets("LoadingScreen/GUI/layouts/logo.layout", m_ExPanelWidget));
+			m_ImageLogoCorner.SetScreenPos(posLogoX, posLogoY, true);
+			m_ImageLogoCorner.LoadImageFile(0, m_ExCustomLoadingScreenData.LogoPath);
+		}
+		//! Hide vanilla logo
+		else if (!m_ExCustomLoadingScreenData.ShowLogo)
+		{
+			Print(ToString() + "::LoadingScreen - Hide logo!");
+			m_ImageLogoCorner.Show(false);
+		}
+
+		if (m_ExCustomLoadingScreenData.LoadingBarColor != 0)
+		{
+			float posLoadingBarX, posLoadingBarY;
+			m_ProgressLoading.GetScreenPos(posLoadingBarX, posLoadingBarY);
+			m_ProgressLoading.Show(false);
+			m_ProgressLoading.Unlink();
+
+			m_ProgressLoading = ProgressBarWidget.Cast(game.GetLoadingWorkspace().CreateWidgets("LoadingScreen/GUI/layouts/loading_bar.layout", m_ExPanelWidget));
+			m_ProgressLoading.SetScreenPos(posLoadingBarX, posLoadingBarY, true);
+			m_ProgressLoading.SetColor(m_ExCustomLoadingScreenData.LoadingBarColor);
+			ProgressAsync.SetProgressData(m_ProgressLoading);
+
+			Print(ToString() + "::LoadingScreen - New progress bar: " + m_ProgressLoading.ToString());
+		}
+
+		Print(ToString() + "::LoadingScreen - End");
+	}
+
+	//! Method called when loading progress has finished.
+	override void Dec()
+	{
+		Print(ToString() + "::Dec - Start");
+
+		super.Dec();
+
+		Print(ToString() + "::Dec - End");
+	}
+
+	override void EndLoading()
+	{
+		Print(ToString() + "::EndLoading - Start");
+
+		super.EndLoading();
+
+		Print(ToString() + "::EndLoading - End");
+	}
+
+	override void OnUpdate(float timeslice)
+	{
+		super.OnUpdate(timeslice);
+
+		m_ExLoadingTime += timeslice;
+		if (m_ExLoadingTime >= LOADING_SCREEN_CHANGE_TIME)
+		{
+			SetBackgroundImage(m_ExBackgroundIndex);
+			m_ExBackgroundIndex++;
+			if (m_ExBackgroundIndex > (m_ExCustomLoadingScreenData.m_ExBackgrounds.Count() - 1))
+				m_ExBackgroundIndex = 0;
+
+			m_ExLoadingTime = 0;
+		}
+	}
+
+	override void ShowEx(DayZGame game)
+	{
+		Print(ToString() + "::ShowEx - Start");
+
+		super.ShowEx(game);
+
+		float loadingTime = m_ExLoadingTime;
+		float tickTime = game.GetTickTime();
+
+		if (m_ExLoadingTimeStamp < 0)
+		{
+			m_ExLoadingTime = 0;
+		}
+		else
+		{
+			m_ExLoadingTime += tickTime - m_ExLoadingTimeStamp;
+		}
+
+		m_ExLoadingTimeStamp = tickTime;
+
+		if (m_ExLoadingTime >= LOADING_SCREEN_CHANGE_TIME)
+		{
+			SetBackgroundImage(m_ExBackgroundIndex);
+			m_ExBackgroundIndex++;
+			if (m_ExBackgroundIndex > (m_ExCustomLoadingScreenData.m_ExBackgrounds.Count() - 1))
+				m_ExBackgroundIndex = 0;
+
+			m_ExLoadingTime = 0;
+		}
+
+		Print(ToString() + "::ShowEx - End");
+	}
+
+	override void Show()
+	{
+		Print(ToString() + "::Show - Start");
+
+		super.Show();
+
+		Print(ToString() + "::Show - End");
+	}
+
+	override void Hide(bool force)
+	{
+		Print(ToString() + "::Hide - Start");
+
+		super.Hide(force);
+
+		Print(ToString() + "::Hide - End");
+	}
+
+	protected void SetBackgroundImage(int index)
+	{
+		Print(ToString() + "::SetBackgroundImage - Start");
+
+		CustomLoadingScreenBackground background = m_ExCustomLoadingScreenData.m_ExBackgrounds.Get(index);
+		if (background && background.ImagePath != string.Empty)
+		{
+			Print(ToString() + "::SetBackgroundImage - Background: " + background.ImagePath);
+			m_ImageWidgetBackground.LoadImageFile(0, background.ImagePath, true);
+			m_ImageWidgetBackground.SetImage(0);
+		}
+
+		Print(ToString() + "::SetBackgroundImage - End");
+	}
+};
