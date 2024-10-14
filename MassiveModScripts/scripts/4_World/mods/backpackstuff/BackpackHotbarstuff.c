a06d5aaa9532b0d270bb64df9cf5d33d75136498
MassPwnaged <48640714+MassPwnaged@users.noreply.github.com>
2024-10-14 17:58:50 -0500

    Update BackpackHotbarstuff.c

--- MassiveModScripts/scripts/4_World/mods/backpackstuff/BackpackHotbarstuff.c
+++ MassiveModScripts/scripts/4_World/mods/backpackstuff/BackpackHotbarstuff.c
@@ -1,38 +1,32 @@
-//modded class ItemBase
-//{
-//    bool CanChildrenAssignToQuickbar()
-//    {
-//        return false;
-//    }
-//
-//    override bool CanAssignToQuickbar()
-//    {
-//        if (!super.CanAssignToQuickbar())
-//            return true;
-//
-//        ItemBase parent = ItemBase.Cast(GetParent());
-//        if (parent)
-//            return parent.CanChildrenAssignToQuickbar();
-//
-//        return false;
-//    }
-//};
-//modded class AliceBag_ColorBase extends Backpack_Base 
-//{
-//	override bool CanChildrenAssignToQuickbar()
-//    {
-//        return true;
-//    }
-//	
-//	override bool CanAssignToQuickbar()
-//    {
-//        if (!super.CanAssignToQuickbar())
-//            return false;
-//
-//        ItemBase parent = ItemBase.Cast(GetParent());
-//        if (parent)
-//            return parent.CanChildrenAssignToQuickbar();
-//
-//        return true;
-//    }
-//};
+modded class ItemBase
+{
+    bool CanChildrenAssignToQuickbar()
+    {
+        return true;
+    }
+
+    override bool CanAssignToQuickbar()
+    {
+        if (!super.CanAssignToQuickbar())
+            return false;
+
+        if (GetInventory())
+        {
+            InventoryLocation loc = new InventoryLocation;
+            GetInventory().GetCurrentInventoryLocation(loc);
+            ItemBase parent = ItemBase.Cast(loc.GetParent());
+            if (parent)
+                return parent.CanChildrenAssignToQuickbar();
+        }
+
+        return true;
+    }
+};
+
+modded class AliceBag_ColorBase
+{
+    override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
