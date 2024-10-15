eeae27748a249edee77b764118d720ee457bba48
MassPwnaged <48640714+MassPwnaged@users.noreply.github.com>
2024-10-15 05:26:17 -0500

    Added ability for items to not be hotbared from inside backpacks
    Todo
    Make exceptions for attachments like walkietalkies and chemlights and guns on some other bags

--- MassiveModScripts/scripts/4_World/mods/backpackstuff/BackpackHotbarstuff.c
+++ MassiveModScripts/scripts/4_World/mods/backpackstuff/BackpackHotbarstuff.c
@@ -22,10 +22,207 @@ modded class ItemBase
         return true;
     }
 };
-
+//Vanilla Backpacks-----------------------------------------------------------------------------
 modded class AliceBag_ColorBase
 {
     override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class TaloonBag_ColorBase
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class TortillaBag
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class CourierBag
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class class FurCourierBag
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class ImprovisedBag
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class FurImprovisedBag
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class DryBag_ColorBase
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class class HuntingBag
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class MountainBag_ColorBase
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class SmershBag
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class ChildBag_ColorBase
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class LeatherSack_ColorBase 
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class AssaultBag_ColorBase
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class CoyoteBag_ColorBase
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class AliceBag_ColorBase
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class SlingBag_ColorBase
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class ArmyPouch_ColorBase
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class DuffelBagSmall_ColorBase
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class CanvasBag_ColorBase
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class DrysackBag_ColorBase
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+//MMIO Backpacks-----------------------------------------------------------------------------
+modded class MassImprovisedBag_Colorbase
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class MassRainCoatcourier_ColorBase
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class MassRuckBagDesert
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class Massbag
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class MassSlingBag
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class MassMolleBagBlack
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class MassSurvivorBag
+{
+	override bool CanChildrenAssignToQuickbar()
+    {
+        return false;
+    }
+};
+modded class Canvas_Backpack_Base
+{
+	override bool CanChildrenAssignToQuickbar()
     {
         return false;
     }
