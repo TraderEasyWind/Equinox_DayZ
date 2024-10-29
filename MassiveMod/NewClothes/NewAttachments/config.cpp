5051ac4fd4896498523a4c95df0c088ffd9dd870
MassPwnaged <48640714+MassPwnaged@users.noreply.github.com>
2024-10-28 19:16:35 -0500

    Work on porting old MassClothes from legacy MMIO

---
+++ MassiveMod/NewClothes/NewAttachments/config.cpp
@@ -0,0 +1,16 @@
+class CfgPatches
+{
+	class MassiveMod_Clothes_Attachments
+	{
+		units[]=
+		{
+		};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Sounds_Effects",
+			"DZ_Gear_Containers"
+		};
+	};
+};
