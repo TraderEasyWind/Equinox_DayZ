0eaea4660fa18e7bc387eb8c4b8505b2add6ed4f
MassPwnaged <48640714+MassPwnaged@users.noreply.github.com>
2025-02-12 17:06:39 -0600

    removed redundant t4 weapon scriptss and classes

--- MassiveModScripts/scripts/4_World/Firearms/TieredWeapons/T4Firearms.c
+++
@@ -1,7 +0,0 @@
-//class MassiveMod_M16A2_T4_Base: RifleBoltLock_Base{};
-//class MassiveMod_AKS74U_T4: AK74_Base{};
-//class MassiveMod_AK101_T4: AK101_Base{};
-//class MassiveMod_AKM_T4: AKM_Base{};
-//class MassiveMod_SKS_T4: SKS_Base{};
-//class MassiveMod_UMP45_T4: UMP45_Base{};
-//class MassiveMod_MP5K_T4: MP5K_Base{};
