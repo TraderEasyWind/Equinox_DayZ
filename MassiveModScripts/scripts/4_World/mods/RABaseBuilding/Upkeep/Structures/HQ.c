77e541b22bb816387b2ededdd0479e1b30b60c2b
MassPwnaged <48640714+MassPwnaged@users.noreply.github.com>
2025-03-18 23:53:11 -0500

    Update HQ.c

--- MassiveModScripts/scripts/4_World/mods/RABaseBuilding/Upkeep/Structures/HQ.c
+++ MassiveModScripts/scripts/4_World/mods/RABaseBuilding/Upkeep/Structures/HQ.c
@@ -6,26 +6,26 @@ modded class TerritoryHQ : ItemBase
         return 1.4;
     }
 
-	override bool CanReleaseAttachment(EntityAI attachment)
-	{
-		if (attachment && attachment.GetInventory() && GetInventory())
-		{
-			InventoryLocation il = new InventoryLocation();
-			attachment.GetInventory().GetCurrentInventoryLocation(il);
-			if (il.IsValid())
-			{
-				int slot = il.GetSlot();
-
-				// Check if the slot is HQ_Nails
-				if (slot == InventorySlots.GetSlotIdFromString("HQ_Nails"))
-				{
-					return false; // Prevent releasing attachment from HQ_Nails
-				}
-
-				return !GetInventory().GetSlotLock(slot);
-			}
-		}
-		return true;
-	}
+	//override bool CanReleaseAttachment(EntityAI attachment)
+	//{
+	//	if (attachment && attachment.GetInventory() && GetInventory())
+	//	{
+	//		InventoryLocation il = new InventoryLocation();
+	//		attachment.GetInventory().GetCurrentInventoryLocation(il);
+	//		if (il.IsValid())
+	//		{
+	//			int slot = il.GetSlot();
+	//
+	//			// Check if the slot is HQ_Nails
+	//			if (slot == InventorySlots.GetSlotIdFromString("HQ_Nails"))
+	//			{
+	//				return false; // Prevent releasing attachment from HQ_Nails
+	//			}
+	//
+	//			return !GetInventory().GetSlotLock(slot);
+	//		}
+	//	}
+	//	return true;
+	//}
 }
 #endif
