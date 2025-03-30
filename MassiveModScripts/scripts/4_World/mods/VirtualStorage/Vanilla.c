5b2b2df739196bfe3adb22574e3080639cdc9a90
MassPwnaged <48640714+MassPwnaged@users.noreply.github.com>
2025-03-30 14:25:35 -0500

    Update Vanilla.c

--- MassiveModScripts/scripts/4_World/mods/VirtualStorage/Vanilla.c
+++ MassiveModScripts/scripts/4_World/mods/VirtualStorage/Vanilla.c
@@ -175,39 +175,19 @@ modded class ExpansionActionRestoreContents
 	{
 		m_Text = "#Open";
 	}
-
-	override bool ActionCondition( PlayerBase player, ActionTarget target, ItemBase item )
-	{
-		Container_Base container;
-		if (!Class.CastTo(container, target.GetParentOrObject()))
-			return false;
-
-		//! Disallow restore contents action on openable containers if closed (useful for debugging if automatic restore on open failed)
-		if ((container.ExpansionIsOpenable() || container.IsNonExpansionOpenable()) && !container.IsOpen())
-			return false;
-
-		if (!container.Expansion_HasEntityStorage() || !container.Expansion_IsEmptyIgnoringLockedSlots())
-			return false;
-
-		return true;
-	}
 	
-	override void OnStartServer( ActionData action_data )
+	override void OnStartServer(ActionData action_data)
 	{
-		Container_Base container;
-
-		if (!Class.CastTo(container, action_data.m_Target.GetParentOrObject()))
-			return;
-
-		if (container.Expansion_RestoreContents(action_data.m_Player))
+		super.OnStartServer(action_data);
+	
+		ItemBase container;
+		if (Class.CastTo(container, action_data.m_Target.GetParentOrObject()))
 		{
-			// Restoration was successful, call DelayedClose on the container itself
-			container.ScheduleDelayedClose(300000); // Schedule the delayed close
-
-		}
-		else
-		{
-			ExpansionNotification("STR_EXPANSION_ERROR_TITLE", "Could not restore contents from virtual storage").Error(action_data.m_Player.GetIdentity());
+			Container_Base containerBase;
+			if (Class.CastTo(containerBase, container))
+			{
+				containerBase.ScheduleDelayedClose(300000);
+			}
 		}
 	}
 }
