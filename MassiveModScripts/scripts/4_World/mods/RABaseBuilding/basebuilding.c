68b772a30052436dc0f11449166ca419d77bce68
MassPwnaged <48640714+MassPwnaged@users.noreply.github.com>
2025-04-14 18:40:05 -0500

    Update basebuilding.c

--- MassiveModScripts/scripts/4_World/mods/RABaseBuilding/basebuilding.c
+++ MassiveModScripts/scripts/4_World/mods/RABaseBuilding/basebuilding.c
@@ -3,13 +3,13 @@ modded class BaseBuilding
 	override float GetTimeToDecay()
 	{
 		switch (GetMaterialLevel()) {
-			case MaterialLevel.FRAME: return 4.0 * 60.0 * 60.0; //2 hours
-			case MaterialLevel.WOOD: return 24.0 * 60.0 * 60.0; //12 hours
-			case MaterialLevel.METAL: return 36.0 * 60.0 * 60.0; //18 hours
-			case MaterialLevel.CONCRETE: return 48.0 * 60.0 * 60.0; //24 hours
+			case MaterialLevel.FRAME: return 336.0 * 60.0 * 60.0; //2 hours
+			case MaterialLevel.WOOD: return 336.0 * 60.0 * 60.0; //12 hours
+			case MaterialLevel.METAL: return 336.0 * 60.0 * 60.0; //18 hours
+			case MaterialLevel.CONCRETE: return 336.0 * 60.0 * 60.0; //24 hours
 		}
 
-		return 24.0 * 60.0 * 60.0; //24 hours
+		return 336.0 * 60.0 * 60.0; //24 hours
 	}
 }
 modded class RA_Blueprint
