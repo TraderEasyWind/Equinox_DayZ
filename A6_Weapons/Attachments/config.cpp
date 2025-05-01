f5524a371e017ac904ab01669333a473c4eb36f6
lucienxyz <josephalootens@gmail.com>
2025-05-01 05:48:15 -0500

    A6 Weapons

---
+++ A6_Weapons/Attachments/config.cpp
@@ -0,0 +1,27 @@
+class CfgPatches
+{
+	class A6_Attachments
+	{
+		units[]={};
+		requiredVersion=0.1;
+		requiredAddons[]={"DZ_Data"};
+	};
+};
+class CfgMods
+{
+	class A6_Attachments
+	{
+		dir="A6_Attachments";
+		picture="";
+		action="";
+		hideName=1;
+		hidePicture=1;
+		name="A6_Attachments";
+		credits="FutureSixx";
+		author="FutureSixx";
+		authorID="0";
+		version="1.0";
+		extra=0;
+		type="mod";
+	};
+};
